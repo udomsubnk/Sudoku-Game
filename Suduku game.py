cd6c3d32cd5b44c430c28b5fb367a9d8d8eda143
taveenun <taveenun2539@hotmail.com>
2017-03-31 17:19:16 +0700

    First Commit

---
+++ Suduku game.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+class Main:
+      def __init__(self,master):
+            frame = Frame(master)
+            frame = Frame(master,background="white")
+            frame.pack()
+            master.title('SUDOKU')
+      
+            
+
+if __name__=='__main__':
+      root=Tk()
+      Main(root)
+      root.mainloop()
+      
+      
+                  
+                  
